6b1078e82e5d11102a4ed642c67608b35d88011d
Mariana Dobrivska <dobrivska.marry@gmail.com>
2023-08-16 09:24:16 +0300

    Initialize project

---
+++ app/components/auth-btn-server.tsx
@@ -0,0 +1,11 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import AuthButtonClient from "./auth-btn-client";
+
+export default async function AuthButtonServer() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  return <AuthButtonClient session={session} />;
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,24 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import GitHubButton from "./github-button";
+
+export const dynamic = "force-dynamic";
+
+export default async function login() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex flex-1 justify-center items-center">
+      <GitHubButton />
+    </div>
+  );
+}
